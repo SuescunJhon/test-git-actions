c15a3bcd6be4f0793e84744080eb1e0bfc35e080
Jhon_Universidad <104960124+SuescunJhon@users.noreply.github.com>
2024-05-01 18:38:37 -0500

    refactor: api.test.js disconnect database

--- test/api.test.js
+++ test/api.test.js
@@ -2,12 +2,19 @@ import request from "supertest";
 import app from "../src/app.js";
 import mongoose from "mongoose";
 import { DBHOST } from "../src/config.js"
-import apiModel from "../src/models/api.model.js";
 
 beforeAll(async () => {
     try {
         await mongoose.connect(DBHOST)
         console.log('DB is connected')
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+afterAll(async () => {
+    try {
+        await mongoose.disconnect()
     } catch (error) {
         console.error(error)
     }
